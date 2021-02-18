058a8088aabb4bab6fa94954060635a334665fcc
Matteo Pastorelli <matteo.pastorelli99@gmail.com>
2021-02-18 16:39:20 +0100

    La votla buona

---
+++ 04-command-line-demo/src/main/java/com/luv2code/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+		
+	@GetMapping("/")
+	public String sayHello() {
+		
+		return "Hellow World! Time on server is " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		
+		return "Run a har 5k!";
+		}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		
+		return "Today is your lucky day.";
+		}
+	
+}
